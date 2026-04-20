83671e88457711af72ddc9e21774422b14d1c56e
huhthatsnice <65060552+scwiggle@users.noreply.github.com>
2026-04-19 22:11:34 -0700

    add smooth scrollbars

--- scripts/singletons/SSCS.gd
+++ scripts/singletons/SSCS.gd
@@ -70,7 +70,7 @@ class Settings:
 
 class Modifiers:
 	var hit_time: float = 45.0
-	var hitbox_size: float = 1.28/2
+	var hitbox_size: float = (0.875 + 0.2625)/2
 	var speed: float = 1
 	var no_fail: bool = true
 	var autoplay: bool = false
