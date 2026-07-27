f73ebf099e5930f6dc8b406b91923ca8a6be409c
Laith Hijazi <90116898+Gapva@users.noreply.github.com>
2026-07-27 13:16:40 -0400

    fix initial grid space background color

--- scenes/spaces/grid/space.gd
+++ scenes/spaces/grid/space.gd
@@ -20,6 +20,12 @@ func _transition_to_color(new_color: Color) -> void:
 		TRANSITION_DURATION_SEC
 	)
 
+func _update_colors() -> void:
+	environment.sky.sky_material.sky_horizon_color = target_color
+	environment.sky.sky_material.ground_horizon_color = target_color
+	tiles.mesh.material.albedo_color = target_color
+	layer.mesh.material.albedo_color = target_color
+
 func _on_note_hit(note: Note) -> void:
 	_transition_to_color(note.color)
 
@@ -27,11 +33,11 @@ func _ready() -> void:
 	game_handler = SSCS.game_handler
 	game_handler.note_hit.connect(_on_note_hit)
 
+	target_color = SSCS.settings.color_set[-1]
+	_update_colors()
+
 func _process(_delta: float) -> void:
 	if not transition_tween or not transition_tween.is_running():
 		return
 
-	environment.sky.sky_material.sky_horizon_color = target_color
-	environment.sky.sky_material.ground_horizon_color = target_color
-	tiles.mesh.material.albedo_color = target_color
-	layer.mesh.material.albedo_color = target_color
+	_update_colors()
